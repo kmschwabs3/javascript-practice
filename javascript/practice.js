23c031c45790ffdd158b2b1b954ac797467a7f4e
Krissy <35669945+kmschwabs3@users.noreply.github.com>
2018-11-20 17:21:59 -0600

    conditional statement problem

--- javascript/practice.js
+++ javascript/practice.js
@@ -13,3 +13,24 @@ if (guitar <= myAccountBalance){
 } else {
 	console.log("You can't afford the guitar.");
 }
+
+if (1===1 && 2===2 || 'joe' === 'joe'){
+	console.log('these are both same');
+}
+
+if (1 === 3 || "joe" === "joe"){
+	console.log('one is true');
+}
+
+var catOne = 5;
+var catTwo = 10;
+var catThree = 1;
+var catThreeDisabledHandicap = true;
+
+if ((catOne > catTwo && catOne >catThree) && !catThreeDisabledHandicap){
+	console.log("Cat 1 is the cutest!");
+} else if ((catTwo > catOne && catTwo > catThree) && !catThreeDisabledHandicap){
+	console.log("Cat 2 is the cutest!");
+} else if ((catThree > catOne && catThree > catTwo) || catThreeDisabledHandicap) {
+	console.log("Cat 3 is the cutest!");
+}
